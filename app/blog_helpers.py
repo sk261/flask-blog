a7781ca41788ef877092143947d0d725b769dd3b
sk261 <sk261@humboldt.edu>
2020-01-29 14:52:24 -0800

    Class commit - Added all page and editor page basic framework.

--- app/blog_helpers.py
+++ app/blog_helpers.py
@@ -1,18 +1,28 @@
 import os
 
-def render_page(file_name, dir_path = 'app/views'):
-    """Takes the specified file path and
-    returns it as HTML
-    """
-    html = ""
-
-    #os.path.join creates an OS-valid path
+_VIEWS_PATH = 'app/views'
+def _set_path(file_name, dir_path_append):
+    dir_path = _VIEWS_PATH
+    if not dir_path_append is None:
+         dir_path = os.path.join(dir_path, dir_path_append)
     path = os.path.join(dir_path, file_name)
+    return path
+
+def render_page(file_name, dir_path_append = None):
+    path = _set_path(file_name, dir_path_append)
+    html = ""
     with open(path) as html_file:
         html = html_file.read()
     return html
 
-def page_exists(file_name, dir_path = 'app/views'):
-    html = ""
-    path = os.path.join(dir_path, file_name)
+def page_exists(file_name, dir_path_append = None):
+    path = _set_path(file_name, dir_path_append)
     return os.path.isfile(path)
+
+def get_page_files():
+    files = []
+    for (path, dn, fn) in os.walk(_VIEWS_PATH):
+        for n in fn:
+            files.append(n)
+    # Next step: Look in SQL Database for descriptions
+    return files
--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,53 @@
 from app import app
-from app.blog_helpers import render_page
-from app.blog_helpers import page_exists
+import app.blog_helpers as BH
 
 #home page
 @app.route("/")
 def home():
-    return render_page('index.html')
+    return BH.render_page('index.html')
+
+@app.route("/edit/<view_name>", methods=['GET', 'POST'])
+def edit(view_name):
+    edit_page = static_file(view_name)
+    page = static_file('edit').replace('LOAD',edit_page)
+
+    ## For when we're ready to start editing stuff
+#    if request.method == 'POST':
+#        pass
+
+    return page
+    
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        pass
+#        session['user_name'] = request.values['user_name']
+    return ""
+
+@app.route("/all")
+def all():
+    page = BH.render_page('all.html')
+    # Get all pages
+    files = BH.get_page_files()
+    insert = ""
+    for n in files:
+        path = '/' + n
+        if n.endswith('index.html'):
+            path = '/'
+        elif n.endswith('.html'):
+            path = '/' + n[:n.rfind('.')]
+        insert += "<tr>"
+        insert += "<th>" + n + "</th>"
+        insert += "<th><a href='" + path + "'>" + path + "</a></th>"
+        insert += "</tr>"
+    page = page.replace("<!--FILL-->", insert)
+    return page
 
 @app.route('/<view_name>')
 def static_file(view_name):
-    if page_exists(view_name + '.html'):
-        return render_page(view_name + '.html')
-    return render_page('404.html')
+    if view_name.endswith('.css'):
+        if BH.page_exists(view_name, 'styles'):
+            return BH.render_page(view_name, 'styles')
+    elif BH.page_exists(view_name + '.html'):
+        return BH.render_page(view_name + '.html')
+    return BH.render_page('404.html')
