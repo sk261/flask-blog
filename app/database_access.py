daecdaf618171de225331e77dbf04ed5dbaf8654
sk261 <sk261@humboldt.edu>
2020-01-31 23:29:23 -0800

    Rudimentary DB design, groundwork for DB access made.

--- app/database_access.py
+++ app/database_access.py
@@ -1,8 +1,29 @@
-'''
-DB manager in Python
-sqlite3:
 import sqlite3
-conn = sqlite3.connect('example.db')
-c = conn.cursor()
-c.execute("INSERT INTO stocks VALUES ('2006-01-05','BUY','RHAT',100,35.14)")
+_DB_FullPath = 'web_pages.db'
+_Connection = sqlite3.connect(_DB_FullPath)
+_Cusor = _Connection.cursor()
+
 '''
+DB Layout
+Key:
+PID - PageID
+UID - UserID
+TID - Thread ID
+P_ID - Post ID
+DOC - Date of Creation
+Icon - Path to Icon image
+    PAGES:PID|Name
+        PAGES_DATA:PID|Path|LoginReq|Description|Icon
+        PAGES_CONTENT:PID|Content
+        PAGES_HISTORY:PID|UID|ChangeDTG|Content
+    USERS:UID|Username
+        USERS_DATA:UID|PW|Email|Verified|DOC|Admin
+    FORUM:TID|ThreadName
+        FORUM_THREAD_DATA:TID|Visible|DOC|UID|Content
+        FORUM_POSTS:TID|P_ID|UID
+            FORUM_POST_DATA:P_ID|UID|DOC|Content|Visible
+'''
+
+def get_data(table, ID):
+    _Cursor.execute('')
+    return ""
